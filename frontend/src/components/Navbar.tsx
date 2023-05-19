e0a0eaf2239eb063684a5a509dc2856c15dc4bb0
JBalila <91769667+JBalila@users.noreply.github.com>
2023-05-19 11:54:02 -0400

    Updated Navbar component to have a 'left-side' and 'right-side'

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,17 +2,21 @@ export default function Navbar({page}: {page: string}) {
     return(
         <div id='navbar-wrapper'>
             <div className='flex-container-row'>
-                <a id={page === 'Home' ? 'active' : ''} className='nav-element' href='/'>
-                    <p>Home</p>
-                </a>
+                <div className='left-side'>
+                    <a id={page === 'Home' ? 'active' : ''} className='nav-element' href='/'>
+                        <p>Home</p>
+                    </a>
+                </div>
 
-                <a id={page === 'FileUpload' ? 'active' : ''} className='nav-element' href='/uploadfiles'>
-                    <p>Upload Files</p>
-                </a>
-                
-                <a id={page === 'AskGPT' ? 'active' : ''} className='nav-element' href='/askgpt'>
-                    <p>Ask GPT</p>
-                </a>
+                <div className='right-side'>
+                    <a id={page === 'FileUpload' ? 'active' : ''} className='nav-element' href='/uploadfiles'>
+                        <p>Upload Files</p>
+                    </a>
+                    
+                    <a id={page === 'AskGPT' ? 'active' : ''} className='nav-element' href='/askgpt'>
+                        <p>Ask GPT</p>
+                    </a>
+                </div>
             </div>
         </div>
     );
